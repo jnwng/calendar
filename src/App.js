b722b57da175cdc6af997be99e968ff2c6a4b742
Jon Wong <jon@coursera.org>
2015-06-23 00:49:27 -0700

    12:04 AM -> 12:49 AM: The first forty-five minutes.
    - Layout of current month.
    - Display of last days of last month
    - Introduction of Square and Calendar components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import moment from 'moment';
+
+class Square extends Component {
+  static propTypes = {
+    width: React.PropTypes.number
+  }
+
+  // getDefaultProps() {
+  //   return {
+  //     width: 100
+  //   }
+  // }
+
+  render() {
+    var style = {
+      backgroundColor: this.props.backgroundColor,
+      display: 'flex',
+      width: `14%`,
+      height: `100px`
+    };
+    return (
+      <div style={style}>
+        {this.props.children}
+      </div>
+    )
+
+  }
+}
+
+export class Calendar extends Component {
+  render() {
+    var daysInMonth = moment().daysInMonth();
+    var firstDayOfMonth = moment().startOf('month');
+    var lastDayOfMonth = moment().endOf('month');
+
+    var lastMonthOffset = firstDayOfMonth.day();
+    var nextMonthOffset = 7 - lastDayOfMonth.day();
+
+    var totalDays = lastMonthOffset + daysInMonth + nextMonthOffset;
+
+    var numberOfWeeks = Math.ceil(totalDays / 7);
+
+    return (
+      <div style={{display: 'flex', flexFlow: 'row wrap'}}>
+        {_(lastMonthOffset).range().reverse().map(offset => {
+          var date = moment().date(-offset);
+          return (
+            <Square key={date.dayOfYear()} backgroundColor='grey'>
+              {date.format('D')}
+            </Square>
+          );
+        }).value()}
+        {_(daysInMonth).range().map(index => {
+          var date = moment().date(index+1);
+          return (
+            <Square key={date.dayOfYear()} backgroundColor='red'>
+              {date.format('D')}
+            </Square>
+          );
+        }).value()}
+      </div>
+    );
+    return <h3>Hello World.</h3>;
+  }
+}
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <Calendar/>
     );
   }
 }
