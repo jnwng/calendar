177c6b5375b17204341dee7c242276d2a6f63f9f
Jon Wong <jon@coursera.org>
2015-06-24 00:04:19 -0700

    11:18 PM -> 12:03 AM, the second forty-five minutes.
    - Added the ability to move between months.
    - Added the row of squares for the day header
    - Added the next month's days.
    - Calendars now accept a "month" prop which is a moment (but should probably be an index moving forward)

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ class Square extends Component {
     width: React.PropTypes.number
   }
 
-  // getDefaultProps() {
-  //   return {
-  //     width: 100
-  //   }
-  // }
-
   render() {
     var style = {
       backgroundColor: this.props.backgroundColor,
@@ -30,13 +24,18 @@ class Square extends Component {
 }
 
 export class Calendar extends Component {
+
+  static propTypes = {
+    month: React.PropTypes.instanceOf(moment)
+  }
+
   render() {
-    var daysInMonth = moment().daysInMonth();
-    var firstDayOfMonth = moment().startOf('month');
-    var lastDayOfMonth = moment().endOf('month');
+    var daysInMonth = this.props.month.daysInMonth();
+    var firstDayOfMonth = this.props.month.clone().startOf('month');
+    var lastDayOfMonth = this.props.month.clone().endOf('month');
 
     var lastMonthOffset = firstDayOfMonth.day();
-    var nextMonthOffset = 7 - lastDayOfMonth.day();
+    var nextMonthOffset = lastDayOfMonth.day() + 1;
 
     var totalDays = lastMonthOffset + daysInMonth + nextMonthOffset;
 
@@ -44,8 +43,15 @@ export class Calendar extends Component {
 
     return (
       <div style={{display: 'flex', flexFlow: 'row wrap'}}>
+        {_(moment.weekdays()).map((day, index) => {
+          return (
+            <Square key={index} backgroundColor='green'>
+              {day}
+            </Square>
+          );
+        }).value()}
         {_(lastMonthOffset).range().reverse().map(offset => {
-          var date = moment().date(-offset);
+          var date = this.props.month.clone().date(-offset);
           return (
             <Square key={date.dayOfYear()} backgroundColor='grey'>
               {date.format('D')}
@@ -53,23 +59,58 @@ export class Calendar extends Component {
           );
         }).value()}
         {_(daysInMonth).range().map(index => {
-          var date = moment().date(index+1);
+          var date = this.props.month.date(index+1);
           return (
             <Square key={date.dayOfYear()} backgroundColor='red'>
               {date.format('D')}
             </Square>
           );
         }).value()}
+
+        {_(7 - nextMonthOffset).range().map(offset => {
+          var date = this.props.month.clone().date(daysInMonth + offset + 1);
+          return (
+            <Square key={date.dayOfYear()} backgroundColor='grey'>
+              {date.format('D')}
+            </Square>
+          );
+        }).value()}
       </div>
     );
-    return <h3>Hello World.</h3>;
   }
 }
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      month: moment().month()
+    };
+  }
+
+  showNextMonth() {
+    this.setState({
+      month: this.state.month + 1
+    });
+  }
+
+  showLastMonth() {
+    this.setState({
+      month: this.state.month - 1
+    });
+  }
+
   render() {
+    var monthMoment = moment().month(this.state.month);
     return (
-      <Calendar/>
+      <div>
+        <Calendar month={monthMoment} />
+        <button onClick={this.showLastMonth.bind(this)}>Decrement</button>
+        <span>{monthMoment.format('MMMM')}</span>
+        <button onClick={this.showNextMonth.bind(this)}>Increment</button>
+      </div>
     );
   }
 }
